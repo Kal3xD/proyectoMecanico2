b60e7541b7ac4e482daa27a03f4bba2eee01c5a4
Diego.Cornejo <diego_ig18@hotmail.com>
2023-06-14 10:49:08 -0400

    subiendo anterior

--- taller/views.py
+++ taller/views.py
@@ -5,3 +5,24 @@ from django.shortcuts import render
 def index(request):
     context={}
     return render(request, 'taller/index.html', context)
+
+
+def chequeoMotor(request):
+    context={}
+    return render(request, 'taller/chequeoMotor.html', context)
+
+def contacto(request):
+    context={}
+    return render(request, 'taller/contacto.html', context)
+
+def nuevas_atenciones(request):
+    context={}
+    return render(request, 'taller/nuevas_atenciones.html', context)
+
+def sistemaElectronico(request):
+    context={}
+    return render(request, 'taller/sistemaElectronico.html', context)
+
+def jsDireccionales(request):
+    context={}
+    return render(request, 'taller/jsDireccionales.html', context)
