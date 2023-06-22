8d4e50ae3898c9b05f8771d2180ebc5595531007
Diego.Cornejo <diego_ig18@hotmail.com>
2023-06-21 20:26:27 -0400

    ultimo caleo main

--- taller/urls.py
+++ taller/urls.py
@@ -1,6 +1,14 @@
 from django.urls import path
 from . import views
 
+
+
 urlpatterns = [
-    path('index', views.index, name = 'index')
+    path('index', views.index, name='index'),
+    path('chequeoMotor', views.chequeoMotor, name='chequeoMotor'),
+    path('contacto', views.contacto, name='contacto'),
+    path('jsDireccionales', views.jsDireccionales, name='jsDireccionales'),
+    path('nuevas_atenciones', views.nuevas_atenciones, name='nuevas_atenciones'),
+    path('sistemasElectronico', views.sistemaElectronico, name='sistemaElectronico')
+
 ]
